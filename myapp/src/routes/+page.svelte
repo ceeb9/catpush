3389402475ded8b82c58a73d6d2bd446378c710c
derinnereSchweinehund <126207001+derinnereSchweinehund@users.noreply.github.com>
2023-03-03 23:08:25 +1100

    created skeleton svelte app

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<style>
+    p {
+        color:pink;
+        font-family:'Courier New', Courier, monospace;
+        font-size: 2em;
+    }
+    h1 {
+        text-align: center;
+    }
+    body {
+        background-color: pink;
+    }
+</style>
+
+<script>
+	let count = 0;
+
+	function increment() {
+		count += 1;
+	}
+</script>
+
+<body>
+<h1>catPush</h1>
+<p>Visit <a href="https://github.com/ceeb9/catpush">CATPUSH</a> to see the repo</p>
+
+
+
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<footer> Currently, the button has been clicked {count} time(s)</footer>
+</body>
