4aa7b2af7e7c01fd648e9026ece95c3dc556dd2a
purplecoffinman <wopovoho775@gmail.com>
2023-03-04 16:31:32 +1100

    buttons and textboxes added to pages

--- myapp/src/routes/uploadPage/+page.svelte
+++ myapp/src/routes/uploadPage/+page.svelte
@@ -1,15 +1,15 @@
 <body>
     <h1>UPLOAD</h1>
+    <p><label for="">You:</label></p>
+    <textarea id="UserID" name="User ID" rows="0" cols="8"></textarea>
+    
+    <p><label for="">Receiver:</label></p>
+    <textarea id="SenderID" name="Sender ID" rows="0" cols="8"></textarea>
+    <button onclick="location.href='/uploadCompletion';">Download</button>
 </body>
 
 <style>
     body {
         background-color: pink;
     }
-    p {
-        color:pink;
-        font-family:'Courier New', Courier, monospace;
-        font-size: 2em;
-    }
 </style>
-
