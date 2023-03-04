9ad751e70627f501eee9fce700898288570d454d
yada2x <lawrenceltnguyen@gmail.com>
2023-03-04 17:09:34 +1100

    Finish header

--- myapp/src/routes/+layout.svelte
+++ myapp/src/routes/+layout.svelte
@@ -2,37 +2,39 @@
     <div class="header">
         <div class="logo">
             <img src="./catlogo.jpg" alt="cat logo">   
-        </div> 
-        <div class="title">
             <p> catpush </p>
         </div>
+        <div class="about"> <p> about </p> </div>
     </div>
-
 </nav>
 
 <style>
     p {
-        font-size: 64px;
+        font-size: 50px;
         font-family: 'VT323';
         font-weight: bolder;
     }
 
     nav {
         margin: 20px;
+        margin-left: 40px;
+        margin-right: 40px;
     }
 
     img {
-        height: 200px;
+        height: 150px;
         width: auto;
     }
 
     .header {
         display: flex;
+        justify-content: space-between;
     }
 
-    .title {
+    .logo {
         display: flex;
         justify-content: center;
+        gap: 60px;
     }
 </style>
 
