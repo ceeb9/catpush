f658dd86543fa21fcb0f15eeb604a7d94a9fa6cf
ceeb <ceeb9999@gmail.com>
2023-03-05 00:17:40 +1100

    update room populations when users leave

--- websocket/server.js
+++ websocket/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(express)
 const wss = new WebSocket.Server({server})
 
 let clients = []
-let rooms = []
+let roomPopulations = {}
 
 //when there is a connection to the web socket server
 wss.on('connection', function connection(ws, req) {
@@ -15,28 +15,26 @@ wss.on('connection', function connection(ws, req) {
     //get the requested room from the client's ws server request string. eg. ws://localhost:8080/{room name}
     //add new room to list of rooms if it doesn't exist
     let currentRoom = req.url.substring(1)
-    if (!rooms.includes(currentRoom)) {
-        rooms.push(currentRoom)
+    if (!(currentRoom in roomPopulations)) {
+        console.log("creating new room " + currentRoom)
+        roomPopulations[currentRoom] = 1
+    } else {
+        roomPopulations[currentRoom] += 1
     }
 
     //create a new client object
+    //make client the room host if the population of the room is 1
     clients.push(new Object({
         websocket: ws,
         room: currentRoom,
-        host: rooms.includes(currentRoom) ? false : true,
+        host: roomPopulations[currentRoom] === 1 ? true : false,
     }))
     console.log("new connection to room " + currentRoom)
 
     //reply with info about the current room
-    let clientsInRoom = 0
-    clients.forEach(client => {
-        if (client.room === currentRoom) {
-            clientsInRoom++
-        }
-    })
     const msg = {
         type: "message",
-        content: ("connected. there are currently " + clientsInRoom + " clients in room " + currentRoom)
+        content: ("connected. there are currently " + roomPopulations[currentRoom] + " clients in room " + currentRoom)
     }
     ws.send(JSON.stringify(msg))
 
@@ -44,9 +42,10 @@ wss.on('connection', function connection(ws, req) {
     ws.on('message', function incoming(data) {
         let parsedData = JSON.parse(data)
 
+        //if the client sends a regular message
         if (parsedData.type === "message") {
             clients.forEach(client => {
-                //if any known clients are in the same room as the message sender, send the message to them
+                //send the message to others in their room
                 if (client.room === currentRoom) {
                     client.websocket.send(JSON.stringify(parsedData))
                 }
@@ -54,10 +53,20 @@ wss.on('connection', function connection(ws, req) {
         }
 
         //if the client sends a closing message
-        //remove them from the list of clients
         if (parsedData.type === "closing") {
             const index = clients.findIndex(x => x.websocket === ws)
-            console.log("client disconnected from room " + clients[index].room)
+            console.log("client index " + index + " is disconnecting")
+
+            //remove the room listing of their room if its population has become 1 
+            //(just them, who is about to leave)
+            console.log("client disconnecting from room " + clients[index].room + ", " + (roomPopulations[clients[index].room]-1).toString() + " people left in that room.")
+            if (roomPopulations[clients[index].room] === 1) {
+                delete roomPopulations[clients[index].room]
+                console.log("deleted room " + currentRoom + ".")
+            } else {
+                roomPopulations[clients[index].room] -= 1
+            }
+            
             clients.splice(index, 1)
         }
         
